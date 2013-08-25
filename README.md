df3f8f66136ee44956c711f817ce9e4baf99e7d4
IPa <ipapamagic@gmail.com>
2013-08-26 00:21:09 +0800

    add git integrate with testflight script

--- README.md
+++ README.md
@@ -32,9 +32,18 @@ xcodeScript_uploadToTestFlight.sh
 
 this script will automatically upload product to testflight
 
-    bash xcodeScript_uploadToTestFlight.sh -a api_token -t team_token
+    bash xcodeScript_uploadToTestFlight.sh -a api_token -t team_token -g git_tag_prefix
 
 get api_token and team_token from  [TestFlight Upload API][1]
+
+-g is an optional argument,this will integrate with git,each time it upload to testflight
+
+this script will automatically add a tag with prefix (argument come with -g) 
+
+and will catch commit message from last tag with prefix to current commit
+
+and send to testflight as update note
+
 
   [1]: https://testflightapp.com/api/doc/ "TestFlight"
 
--- xcodeScript_uploadToTestFlight.sh
+++ xcodeScript_uploadToTestFlight.sh
@@ -11,17 +11,35 @@
 plist="$PROJECT_DIR/$INFOPLIST_FILE"
 buildVersionNum=$(/usr/libexec/Plistbuddy -c "Print CFBundleVersion" "$plist")
 if [ -z "$buildVersionNum" ]; then
-echo "No build number in $plist"
+    echo "No build number in $plist"
 exit 2
 fi
-while getopts a:t: option
+while getopts a:t:g: option
 do
-case "${option}"
-in
-a) API_TOKEN=${OPTARG};;
-t) TEAM_TOKEN=${OPTARG};;
-esac
+    case "${option}"
+    in
+    a) API_TOKEN=${OPTARG};;
+    t) TEAM_TOKEN=${OPTARG};;
+    g) GIT_TAG_PREFIX=${OPTARG};;
+    esac
 done
+#generate note from git
+COMMIT_LOG="Build uploaded automatically from Xcode."
+if [ ! -z "$GIT_TAG_PREFIX" ]; then
+    cd "${PROJECT_DIR}"
+    targetTag=$(git describe --match "$GIT_TAG_PREFIX*")
+    if [[ -z $targetTag ]]
+    then
+        COMMIT_LOG=$(git log --reverse --format="%b %s")
+    else
+        IFS="-" read -a array <<< "$targetTag"
+        targetTag="${array[0]}"
+        COMMIT_LOG=$(git log --reverse --format="%b %s" $targetTag..HEAD)
+    fi
+    git tag -a "$GIT_TAG_PREFIX$buildVersionNum" -m "$GIT_TAG_PREFIX$buildVersionNum"
+    echo "add tag $GIT_TAG_PREFIX$buildVersionNum to current commit"
+fi
+
 #zip dsym
 #DATE=`date +%Y%m%d`
 ZIPPED_DSYM_OUTPUT_FILE_PATH="${PROJECT_DIR}/${PRODUCT_NAME}.dsym.zip"
@@ -36,8 +54,9 @@ xcrun -sdk iphoneos PackageApplication "$appPath" -o "$ipaPath" --sign "${CODE_S
 -F file=@"$ipaPath" \
 -F api_token="${API_TOKEN}" \
 -F team_token="${TEAM_TOKEN}" \
--F notes="Build uploaded automatically from Xcode." \
+-F notes="$COMMIT_LOG" \
 -F dsym=@"$ZIPPED_DSYM_OUTPUT_FILE_PATH"
+
 #delete ipa
 rm "$ipaPath"
 
